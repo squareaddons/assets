b9e6b1247847b13840513c930cf45d149dcc7d86
squareaddons <63316439+squareaddons@users.noreply.github.com>
2020-04-15 16:56:17 +0300

    Create slideshow.js

---
+++ plugins/slideshow/slideshow.js
@@ -0,0 +1,23 @@
+var showBullets = true; // set false if you wnat to remove the dots
+  
+document.querySelectorAll('.sqs-gallery-design-list .summary-item-list')[0].classList.add("custom-slider");
+  
+var sliderDetail = tns({
+  container: '.custom-slider',
+  items: 1,
+  gutter: 20,
+  nav: showBullets,
+  controls: false,
+  loop: true,
+  mouseDrag: true,
+  controlsPosition: 'bottom',
+  prevButton: false,
+  autoplayButton: false,
+  autoplayHoverPause: true,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayButtonOutput: false,
+  controlsText: ['', ''],
+  edgePadding: 0,
+  navPosition: 'bottom'
+});
