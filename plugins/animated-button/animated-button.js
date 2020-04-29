baa9ce313d5038358d6a266a1e7e10586f4cbeb9
squareaddons <63316439+squareaddons@users.noreply.github.com>
2020-04-29 23:13:03 +0300

    Update animated-button.js

--- plugins/animated-button/animated-button.js
+++ plugins/animated-button/animated-button.js
@@ -1,6 +1,8 @@
 /* This is script for svg or png elements adding. You can your own element at '' after 'beforeend' */
 
 <script>
-  var button = document.querySelectorAll('.sqs-block-button-element--small')[0];  
-  button.insertAdjacentHTML('beforeend', '<svg width="11" height="11" viewBox="1 -2 10 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.74211 3.96428L1.96379 10.7426L0.549573 9.3284L7.3279 2.55007L1.25668 2.55007L1.25668 0.550373L10.7418 0.550373L10.7418 10.0355L8.74211 10.0355L8.74211 3.96428Z" fill="white"/></svg>');
-</script> 
+  var button = document.querySelectorAll('.sqs-block-button-element--small'); 
+  for( let i = 0; i < button.length; i++){
+    button[i].insertAdjacentHTML('beforeend', '<svg width="11" height="11" viewBox="1 -2 10 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.74211 3.96428L1.96379 10.7426L0.549573 9.3284L7.3279 2.55007L1.25668 2.55007L1.25668 0.550373L10.7418 0.550373L10.7418 10.0355L8.74211 10.0355L8.74211 3.96428Z" fill="white"/></svg>');
+  }
+</script>
